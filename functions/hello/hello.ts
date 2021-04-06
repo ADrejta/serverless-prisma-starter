b933e70017ec2e89b66c68f234baa0c06386bf8f
Aurel Drejta <aureldrejta@gmail.com>
2021-04-06 22:52:02 +0200

    Add types for handler parameters and return types

--- functions/hello/hello.ts
+++ functions/hello/hello.ts
@@ -1,12 +1,11 @@
-import { APIGatewayEvent, Context } from 'aws-lambda';
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { v4 as uuid } from 'uuid';
-import { prisma } from '../../databaseConnections/postgres';
 
-export const helloExport = async (event: APIGatewayEvent, context: Context) => {
+export const helloExport = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
   try {
-    console.log('Hi');
-
-    await prisma.user.findMany();
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -16,6 +15,9 @@ export const helloExport = async (event: APIGatewayEvent, context: Context) => {
   } catch (error) {
     return {
       statusCode: 500,
+      body: JSON.stringify({
+        message: 'Something went wrong!! Please report this',
+      }),
     };
   }
 };
--- functions/hello2/hello2.ts
+++ functions/hello2/hello2.ts
@@ -1,18 +1,25 @@
-import randomstring from "randomstring";
-import { prisma } from "../../databaseConnections/postgres";
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import randomstring from 'randomstring';
+import { prisma } from '../../databaseConnections/postgres';
 
-export const helloExport = async (event) => {
-   try {
-      await prisma.user.findMany()
-      return {
-         statusCode:200,
-         body: JSON.stringify({
-            message: randomstring.generate()
-         })
-      }
-   } catch (error) {      
-      return {
-         statusCode:500
-      }
-   }
-}
+export const helloExport = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  try {
+    await prisma.user.findMany();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: randomstring.generate(),
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Something went wrong!! Please report this',
+      }),
+    };
+  }
+};
